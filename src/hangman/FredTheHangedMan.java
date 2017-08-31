f1f67d015010424c59a15eda2d959c6f201cd5a9
J05H14 <j05h14@yahoo.com>
2017-08-30 17:19:24 -0700

    result and counter up

--- src/hangman/FredTheHangedMan.java
+++ src/hangman/FredTheHangedMan.java
@@ -3,8 +3,27 @@ package hangman;
 public class FredTheHangedMan {
 	
 	private int counter;
+	private int word;
 	
-	public FredTheHangedMan(){
+	public FredTheHangedMan(String word){
 		this.counter = 0;
+		this.word = word;
+	}
+	
+	public void wrong(char letter){
+		if(counter == 6){
+			result(true);
+		}
+		counter++;
+	}
+	
+	
+	public String result(boolean isLoss){
+		if(isLoss == true){
+			return "You Lose! You Suck!";
+		}
+		else{
+			return "You Win... You got lucky...";
+		}
 	}
 }
